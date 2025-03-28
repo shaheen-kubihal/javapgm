2f7c37fd5d5bd08ee7758c6825ccd539010d1d25
ranjitrevanakar02 <ranjitrevanakar02@gmail.com>
2025-03-28 11:03:30 +0530

    save the java file

--- java2.java
+++
@@ -1,6 +0,0 @@
-public class java1 {
-    public static void main(String[] args) {
-        System.out.print.ln("Hello world");
-    }
-
-}
---
+++ javapgm1.java
@@ -0,0 +1,8 @@
+public class javapgm1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("sum is" + (a + b));
+
+    }
+}
